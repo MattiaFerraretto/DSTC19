37f9a6f632220d9e282394249b3aabd51b062957
MattiaFerraretto <57230458+MattiaFerraretto@users.noreply.github.com>
2021-10-30 16:05:50 +0200

    Update pre_process.py

--- pre_process.py
+++ pre_process.py
@@ -1,6 +1,6 @@
 """
 DSTC19
-Copyright (C) 2021  Mattia Ferraretto <ferrar3tto.mattia@gmail.com>
+Copyright (C) 2021  Mattia Ferraretto <ferraretto.mattia@protonmail.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
